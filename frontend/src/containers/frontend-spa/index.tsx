07ec55f91c92cf1e29c45c096e0273f249ea55e2
zhsusn@163.com <zhsusn@163.com>
2026-06-13 14:35:42 +0800

    fix(arch): 【根因分析】 （根因分析未能完成：）
    【修复方案】
    容器 'frontend-spa' 在 C4

---
+++ frontend/src/containers/frontend-spa/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export interface FrontendSpaProps {
+  // TODO: define props
+}
+
+/**
+ * TODO: implement frontend-spa container
+ */
+export const FrontendSpa: React.FC<FrontendSpaProps> = () => {
+  return <div>frontend-spa</div>
+}
+
+export default FrontendSpa
